4057504d27a8fae0defd92ba8aaea8109a5309f2
Jordi Canals <jordicanalsros@gmail.com>
2014-06-01 11:55:25 +0200

    Init

---
+++ src/com/example/TestMidlet.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.example;
+
+import javax.microedition.midlet.MIDlet;
+
+/**
+ *
+ * @author Jordi Canals <jordicanalsros@gmail.com>
+ */
+public class TestMidlet extends MIDlet {
+    
+    @Override
+    public void startApp() {
+    }
+    
+    @Override
+    public void destroyApp(boolean unconditional) {
+    }
+}
